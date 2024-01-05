fa52c6116fda6df259e090d5d0f97344db9189f0
Christian Veigt <cveigt@gmail.com>
2024-01-04 18:03:28 -0800

    Add `postMessage` and `onmessage` event handlers

--- string-generator/code.ts
+++ string-generator/code.ts
@@ -9,9 +9,17 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
+figma.ui.onmessage = pluginMessage => {
+  console.log(pluginMessage.dataType);
+  
+  figma.closePlugin();
+}
+
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
+
+/*
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
@@ -26,9 +34,10 @@ figma.ui.onmessage = msg => {
     }
     figma.currentPage.selection = nodes;
     figma.viewport.scrollAndZoomIntoView(nodes);
-  }
+  } 
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
   figma.closePlugin();
 };
+ */
